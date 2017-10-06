50dcaa53ec3c31950a158b5d1a1b120a2895713f
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    Fixed all pawn move failings.

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -5,14 +5,23 @@ import Piece from './piece';
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
+
     }
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+            if (this.hasPieceMoved) {
+                return [Square.at(location.row + 1, location.col)] 
+            } else {
+                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+            }
         } else {
-            return Square.at(location.row - 1, location.col)
+            if (this.hasPieceMoved) {
+                return [Square.at(location.row - 1, location.col)] 
+            } else {
+                return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+            }
         }
     }
 }
