2ff37c9227b4351cfed3154072badab9c1d3c51f
Amy Sun <amy.sun@softwire.com>
2024-07-08 15:55:12 +0100

    not fixed Red-8 yet

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,13 +9,25 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
+        let location = board.findPiece(this);
+
         if (this.player === Player.WHITE) {
-            if (this.hasPieceMoved) {
-                return [Square.at(location.row + 1, location.col)] 
+            let square = Square.at(location.row + 1, location.col);
+            //let isOccupied = board.getPiece(square);
+            let isOccupied = true;
+            if (board.getPiece(square) === undefined) isOccupied = false;
+            console.log("Is Occupied", isOccupied);
+
+            if (!isOccupied) {
+                if (this.hasPieceMoved) {
+                    return [Square.at(location.row + 1, location.col)] 
+                } else {
+                    return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                }
             } else {
-                return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                return []
             }
+            
         } else {
             if (this.hasPieceMoved) {
                 return [Square.at(location.row - 1, location.col)] 
