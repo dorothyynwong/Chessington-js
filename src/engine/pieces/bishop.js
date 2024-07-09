222f9a13a81591459e40ed6b44b51b83eff402bf
Amy Sun <amy.sun@softwire.com>
2024-07-09 15:53:13 +0100

    Fixed Red-9 failings

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,23 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+
+        let moves = [];
+        const board_size = GameSettings.BOARD_SIZE;
+
+        for (let i = 1; i <= board_size; i++) {
+            if (row + i < board_size && col + i < board_size) moves.push(Square.at(row + i, col + i));
+            if (row - i >= 0 && col - i >= 0) moves.push(Square.at(row - i, col - i));
+        }
+
+        for (let j = 1; j <= board_size; j++) {
+            if (row + j < board_size && col - j >= 0) moves.push(Square.at(row + j, col - j));
+            if (row - j >= 0 && col + j < board_size) moves.push(Square.at(row - j, col + j));
+        }
+        console.log(moves);
+        return moves;
     }
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -45,45 +45,4 @@ export default class Rook extends Piece {
 
         return moves;
     }
-
-  /*  getAvailableMoves(board) {
-        let location = board.findPiece(this);
-        let row = location.row;
-        let col = location.col;
-
-        let moves = [];
-
-        for (let i=0; i<8; i++) {
-            let square = Square.at(row, i);
-            let piece = board.getPiece(square);
-            if (col !== i) {
-                if (piece === undefined) moves.push(square);
-                else break;
-            } 
-        }
-        for (let i=0; i<8; i++) {
-            let square = Square.at(i, col);
-            let piece = board.getPiece(square);
-            if (row !== i) {
-                if (piece === undefined) moves.push(square);
-                else break;
-            } 
-            
-        }
-        return moves;
-    } */
 }
-
-
-/*
-  0 1 2 3 4 5 6 7
-7
-6
-5
-4
-3   X R
-2
-1
-0      
-
-*/
