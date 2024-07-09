a10a9ee613fa713bf9f0c23d1cd6538f49678e8e
Amy Sun <amy.sun@softwire.com>
2024-07-09 11:49:17 +0100

    fix Red-9

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -13,28 +13,54 @@ export default class Rook extends Piece {
 
         let moves = [];
 
-   /*     for(let i=0; i<8; i++) {
-            moves.push(Square.at(row, col+i));
-            moves.push(Square.at(row+i, col));
+        for (let i=0; i<8; i++) {
+            // horizontal moves
+            let square = Square.at(row, i);
+            let piece = board.getPiece(square);
+            if (col !== i) {
+                if (piece === undefined) moves.push(square);
+                else break;
+            } 
+
+            // vertical moves
+            square = Square.at(i, col);
+            piece = board.getPiece(square);
+
+            if (row !== i) {
+                if (piece === undefined) moves.push(square);
+                else break;
+            } 
         }
-        
-        let locationToRemove = location
 
-        let legalMoves = moves.filter(item => item.row !== locationToRemove.row || item.col !== locationToRemove.col);
-
-        return legalMoves;*/
-
-        /*      Square.at(1, 0), Square.at(1, 1), Square.at(1, 3), Square.at(1, 4), Square.at(1, 5), Square.at(1, 6), Square.at(1, 7),
-            // Vertical
-            Square.at(0, 2), Square.at(2, 2), Square.at(3, 2), Square.at(4, 2), Square.at(5, 2), Square.at(6, 2), Square.at(7, 2)*/
-
-            for(let i=0; i<8; i++) {
-                if (col !== i) moves.push(Square.at(row, i));
-            }
-            for (let i=0; i<8; i++) {
-                if (row !== i) moves.push(Square.at(i, col));
-            }
-
-            return moves;
+        return moves;
     }
+
+  /*  getAvailableMoves(board) {
+        let location = board.findPiece(this);
+        let row = location.row;
+        let col = location.col;
+
+        let moves = [];
+
+        for (let i=0; i<8; i++) {
+            let square = Square.at(row, i);
+            let piece = board.getPiece(square);
+            if (col !== i) {
+                if (piece === undefined) moves.push(square);
+                else break;
+            } 
+        }
+        for (let i=0; i<8; i++) {
+            let square = Square.at(i, col);
+            let piece = board.getPiece(square);
+            if (row !== i) {
+                if (piece === undefined) moves.push(square);
+                else break;
+            } 
+            
+        }
+        return moves;
+    } */
 }
+
+
