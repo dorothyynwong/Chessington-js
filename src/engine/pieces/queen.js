342f0d287adb73c4f511c901a293e8d387c3bd20
Dorothy Wong <dorothy.wong@softwire.com>
2024-07-10 17:52:50 +0100

    simplify queen.js, using a function instead of 1 loop per direction

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -8,127 +8,48 @@ export default class Queen extends Piece {
         super(player);
     }
 
+ 
+
     getAvailableMoves(board) {
         let location = board.findPiece(this);
         let row = location.row;
         let col = location.col;
         const board_size = GameSettings.BOARD_SIZE;
+        const directions = [
+            [1,0], //North
+            [-1,0], //South
+            [0,1], //East
+            [0,-1], //West
+            [1,1], //NE
+            [1,-1], //NW
+            [-1,1], //SE
+            [-1,-1] //SW
+        ]
 
         let moves = [];
 
-        // moves to right
-        for (let i=col+1; i<board_size; i++) {
-            let square = Square.at(row, i);
-            let piece = board.getPiece(square);
-            if (piece === undefined) moves.push(square);
-            else {
-                if (piece.player !== this.player && !(piece instanceof King)) {
-                    moves.push(square);
-                    break;
-                } else break;
-            }
-        }
-
-        // moves to left
-        for (let i=col-1; i>=0; i--) {
-            let square = Square.at(row, i);
-            let piece = board.getPiece(square);
-            if (piece === undefined) moves.push(square);
-            else {
-                if (piece.player !== this.player && !(piece instanceof King)) {
-                    moves.push(square);
-                    break;
-                } else break;
-            }
-        }
-
-        // moves up
-        for (let i=row+1; i<board_size; i++) {
-            let square = Square.at(i, col);
-            let piece = board.getPiece(square);
-            if (piece === undefined) moves.push(square);
-            else {
-                if (piece.player !== this.player && !(piece instanceof King)) {
-                    moves.push(square);
-                    break;
-                } else break;
-            }
-        }
-
-        // moves down
-        for (let i=row-1; i>=0; i--) {
-            let square = Square.at(i, col);
-            let piece = board.getPiece(square);
-            if (piece === undefined) moves.push(square);
-            else {
-                if (piece.player !== this.player && !(piece instanceof King)) {
-                    moves.push(square);
-                    break;
-                } else break;
-            }
-        }
-
-        
-
-        // diagonal moves
-        for (let i = 1; i <= board_size; i++) {
-            if (row + i < board_size && col + i < board_size) {
-                let square = Square.at(row + i, col + i);
+        //get moves in direction direction
+        const getMovesInDirection = (row_direction, col_direction) => {
+            let r = row+row_direction;
+            let c = col+col_direction;
+            while (r<board_size && c < board_size && r >= 0 && c >= 0)
+            {
+                let square = Square.at(r, c);
                 let piece = board.getPiece(square);
-                if (!piece) {
-                    moves.push(Square.at(row + i, col + i));
+                if (piece === undefined) {
+                    moves.push(square);
                 } else {
                     if (piece.player !== this.player && !(piece instanceof King)) {
                         moves.push(square);
-                        break;
-                    } else break;
-
-                };
-            }
-            if (row - i >= 0 && col - i >= 0) {
-                let square = Square.at(row - i, col - i);
-                let piece = board.getPiece(square);
-                if (!piece) {
-                    moves.push(Square.at(row - i, col - i));
-                } else {
-                    if (piece.player !== this.player && !(piece instanceof King)) {
-                        moves.push(square);
-                        break;
-                    } else break;
-
-                };
-            } 
-                
-        }
-
-        for (let j = 1; j <= board_size; j++) {
-            if (row + j < board_size && col - j >= 0) {
-                let square = Square.at(row + j, col - j);
-                let piece = board.getPiece(square);
-                if (!piece) {
-                    moves.push(Square.at(row + j, col - j));
-                } else {
-                    if (piece.player !== this.player && !(piece instanceof King)) {
-                        moves.push(square);
-                        break;
-                    } else break;
-
-                };
-            } 
-            if (row - j >= 0 && col + j < board_size) {
-                let square = Square.at(row - j, col + j);
-                let piece = board.getPiece(square);
-                if (!piece) {
-                    moves.push(Square.at(row - j, col + j));
-                } else {
-                    if (piece.player !== this.player && !(piece instanceof King)) {
-                        moves.push(square);
-                        break;
-                    } else break;
-
-                };
+                    }
+                    break;
+                }
+                r += row_direction;
+                c += col_direction;
             }
         }
+
+        directions.forEach(([r,c]) => getMovesInDirection(r,c));
 
         return moves
 
